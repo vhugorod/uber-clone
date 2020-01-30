83387f801e6235d686bfa996c7ebf64e4271977a
Vitor Rodrigues <vitorrodrigueshr10@gmail.com>
2020-01-30 01:02:50 -0300

    Maps api connection

--- android/app/src/main/java/com/uberclone/MainApplication.java
+++ android/app/src/main/java/com/uberclone/MainApplication.java
@@ -22,11 +22,10 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+            return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                  new MapsPackage()
+            );
         }
 
         @Override
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 
 import {View} from 'react-native';
 
-// import { Container } from './styles';
-
-const App = () => <View />;
+const App = () => (
+  <View style={{flex: 1}}>
+    <MapView
+      style={{flex: 1}}
+      region={{
+        latitude: -19.7477482,
+        longitude: -47.9438657,
+        latitudeDelta: 0.0143,
+        longitudeDelta: 0.0134,
+      }}
+      showsUserLocation
+      loadingEnabled
+    />
+  </View>
+);
 
 export default App;
